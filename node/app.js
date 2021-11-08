f061956f3cce495c59f9ffa44474a74fc40a6e31
zooksman <zooksman@gmail.com>
2021-11-08 14:25:18 -0600

    adding node express

--- node/app.js
+++ node/app.js
@@ -1,14 +1,23 @@
-const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/lot1', (req, res) => {
+	var blah = {
+		"space1": 0,
+		"space2": 0,
+		"space3": 1,
+		"space4": 0,
+	};
+	res.send(blah);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
