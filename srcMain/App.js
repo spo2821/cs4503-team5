a1b7a8411c545959345178a9318dad0ec9b4a3ca
spo2821 <spo2821@yahoo.com>
2021-11-10 23:00:39 -0600

    code cleanup

---
+++ srcMain/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {router} from 'router.js';
+
+function App(){
+    router();
+
+}
---
+++ srcMain/router.js
@@ -0,0 +1,54 @@
+import React from "react";
+import{
+    BrowserRouter as Router,
+    Routes,
+    
+    Link,
+    useRouteMatch,
+    useParams,
+} from "react-router-dom";
+import { Route, Switch } from "react-router";
+import {SignIn} from "map.js"
+
+export default function router(){
+    return(
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/topics"> Topics </Link>
+                    </li>
+                </ul>
+                <Routes>
+                    <Route path="/about" component={About} />
+                    <Route path="/" component={Home}/>
+                    <Route path="/topics" component={Topics} />
+                    
+                    
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+function Home(){
+    
+        SignIn();
+    
+}
+function About(){
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+}
+function Topics(){
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+}
