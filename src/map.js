c95f0000b251d8d1a4c41bc7020621b3f8acb811
zooksman <zooksman@gmail.com>
2021-11-30 13:21:16 -0600

    max version

--- src/map.js
+++ src/map.js
@@ -1,28 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import './Spots.css'
-import { ModuleResolutionKind } from 'typescript';
 
 
 const suA = [['a1','Closed'],['a2','Closed'],['a3','Closed'],['a4','Open'],['a5','Open'],['a6','Open'],['a7','Closed'],['a8','Closed'],['a9','Closed'],['a10','Closed'],['a11','Closed'],['a12','Closed'],['a13','Closed'],['a14','Closed'],['a15','Open'],['a16','Open'],['a17','Open'],['a18','Open']]
 const suB = [['b1','Open'],['b2','Closed'],['b3','Closed'],['b4','Closed'],['b5','Closed'],['b6','Open'],['b7','Open'],['b8','Open'],['b9','Closed'],['b10','Closed'],['b11','Closed'],['b12','Closed'],['b13','Closed'],['b14','Open'],['b15','Open'],['b16','Open'],['b17','Closed'],['b18','Closed']]
 const suC = [['c1','Closed'],['c2','Closed'],['c3','Open'],['c4','Open'],['c5','Open'],['c6','Closed'],['c7','Closed'],['c8','Closed'],['c9','Closed'],['c10','Closed'],['c11','Closed'],['c12','Open'],['c13','Open'],['c14','Closed'],['c15','Closed']]
-
-var lotNumber;
-
-async function initVars() {
-  const stuff = await fetch('localhost:3000/lot/' + lotNumber);
-  const data = await stuff.json();
-}
-
-
-
 function App() {
-  initVars();
   return (
     <div className="App">
       <h1 class = "Header">Student Union</h1>
-        
+
       <Spots />
       <Map />
     </div>
@@ -30,24 +18,38 @@ function App() {
 }
 
 function Spots(){
-  var row;
   return(
     <div>
-      <div>
-    	  for (row in data ) {
-      
-          <div>
-            {row.map((space, index) => (
-              <div class = {space ? 'Open' : 'Closed'} key={index}>
-            {index}
+    <div>
+       <div>
+          {suA.map((suA, index) => (
+            <div class = {suA[1]} key={index}>
+              {suA[0]}
             </div>
           ))}
-          </div>
-          //<div class = "Break"></div>
-      
-    	  }
-        
-      </div>
+        </div>
+
+
+        <div>
+          {suB.map((suB, index) => (
+            <div class = {suB[1]} key={index}>
+              {suB[0]}
+            </div>
+          ))}
+        </div>
+
+
+        <div class = "Break"></div>
+
+
+        <div class = "Row3">
+          {suC.map((suC, index) => (
+            <div class = {suC[1]} key={index}>
+              {suC[0]}
+            </div>
+          ))}
+        </div>
+    </div>
     </div>
   )
 }
@@ -69,10 +71,4 @@ function Map(){
       </div>
   )
   }
-
- export {
-     App,
-     Spots,
-     Map,
-     
- }
+export default App;
